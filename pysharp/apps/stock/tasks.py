3f4b161a4ec576dc36db960ee354a4fca9cab2ca
LBI <452381072@qq.com>
2017-02-26 16:54:13 +0800

    定时任务处理

--- pysharp/apps/stock/tasks.py
+++ pysharp/apps/stock/tasks.py
@@ -3,12 +3,12 @@ from celery import task
 
 # from task.celery import app
 
-@task(name='add相加')
+@task()
 def add(x, y):
     return x + y
 
 
-@task(name='add2相加')
+@task()
 def add2(x, y):
     return x + y
 
